79075cd1f2d19530f01d5a8f83930c67a61851ab
Mike Scott <mike.scott@nttdata.com>
2020-01-23 00:38:57 +0000

    Setup car steering and acceleration

---
+++ Car.gd
@@ -0,0 +1,66 @@
+extends KinematicBody2D
+
+# Parameters
+var wheel_base = 70
+var steering_angle = 15
+var engine_power = 800
+var braking = -450
+var max_speed_reverse = 250
+
+var friction = -0.9
+var drag = -0.0015
+
+# Variables
+var velocity = Vector2.ZERO
+var steer_angle
+var acceleration = Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#    pass
+
+func _physics_process(delta):
+    get_input()
+    apply_friction()
+    calculate_steering(delta)
+    velocity += acceleration * delta
+    move_and_slide(velocity)
+    
+func get_input():
+    var turn = 0
+    if Input.is_action_pressed("ui_right"):
+        turn += 1
+    if Input.is_action_pressed("ui_left"):
+        turn -= 1
+    steer_angle = turn * deg2rad(steering_angle)
+    
+    acceleration = Vector2.ZERO
+    if Input.is_action_pressed("ui_up"):
+        acceleration = transform.x * engine_power
+    if Input.is_action_pressed("ui_down"):
+        acceleration += transform.x * braking
+
+    
+func calculate_steering(delta):
+    var rear_wheel = position - transform.x * wheel_base / 2.0
+    var front_wheel = position + transform.x * wheel_base / 2.0
+    rear_wheel += velocity * delta
+    front_wheel += velocity.rotated(steer_angle) * delta
+    var new_heading = (front_wheel - rear_wheel).normalized()
+    var d = new_heading.dot(velocity.normalized())
+    if d > 0:
+        velocity = new_heading * velocity.length()
+    if d < 0:
+        velocity = -new_heading * min(velocity.length(), max_speed_reverse)
+    rotation = new_heading.angle()
+    
+func apply_friction():
+    if velocity.length() < 5:
+        velocity = Vector2.ZERO
+    var friction_force = velocity * friction
+    var drag_force = velocity * velocity.length() * drag
+    acceleration += drag_force + friction_force
