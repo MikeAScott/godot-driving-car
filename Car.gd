a947173118db84210a247fc2468ef28445f42048
Mike Scott <mike.scott@nttdata.com>
2020-02-04 00:26:13 +0000

    Tidy up

--- Car.gd
+++ Car.gd
@@ -15,8 +15,8 @@ var high_speed_traction = 0.05
 
 # Variables
 var velocity = Vector2.ZERO
-var steer_angle
-var acceleration = Vector2.ZERO
+#var steer_angle
+#var acceleration = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -27,44 +27,68 @@ func _ready():
 #    pass
 
 func _physics_process(delta):
-    get_input()
-    apply_friction()
-    calculate_steering(delta)
+    if velocity.length() < 5:
+        velocity = Vector2.ZERO   
+    var acceleration = get_acceleration() + get_friction() 
+    var steering = get_steering()
+    var heading = calculate_heading(steering, delta)
+    change_heading(heading)
+    
     velocity += acceleration * delta
     move_and_slide(velocity)
-    
-func get_input():
+ 
+func get_acceleration():
+    var acceleration = Vector2.ZERO
+    if Input.is_action_pressed("ui_up"):
+        acceleration = transform.x * engine_power
+    if Input.is_action_pressed("ui_down"):
+        acceleration = transform.x * braking
+    return acceleration
+
+func get_steering():
     var turn = 0
     if Input.is_action_pressed("ui_right"):
         turn += 1
     if Input.is_action_pressed("ui_left"):
         turn -= 1
-    steer_angle = turn * deg2rad(steering_angle)
+    return turn * deg2rad(steering_angle)    
     
-    acceleration = Vector2.ZERO
-    if Input.is_action_pressed("ui_up"):
-        acceleration = transform.x * engine_power
-    if Input.is_action_pressed("ui_down"):
-        acceleration += transform.x * braking
+#func calculate_steering(delta):
+#    var new_heading = calculate_heading(delta)
+#    var d = new_heading.dot(velocity.normalized())
+#    if d > 0:
+#        velocity = apply_drift_to(new_heading)
+#    if d < 0:
+#        velocity = reverse_to(new_heading)
+#    rotation = new_heading.angle()
+    
+func calculate_heading(steer_angle,delta):
+    var wheel_offset = transform.x * wheel_base / 2.0
+    var rear_wheel = position - wheel_offset + velocity * delta
+    var front_wheel = position + wheel_offset + velocity.rotated(steer_angle) * delta
+    return (front_wheel - rear_wheel).normalized()   
 
-    
-func calculate_steering(delta):
-    var rear_wheel = position - transform.x * wheel_base / 2.0
-    var front_wheel = position + transform.x * wheel_base / 2.0
-    rear_wheel += velocity * delta
-    front_wheel += velocity.rotated(steer_angle) * delta
-    var new_heading = (front_wheel - rear_wheel).normalized()
-    var traction = low_speed_traction if velocity.length() < slip_speed else high_speed_traction
-    var d = new_heading.dot(velocity.normalized())
+func change_heading(heading):
+    var d = heading.dot(velocity.normalized())
     if d > 0:
-        velocity = velocity.linear_interpolate(new_heading * velocity.length(), traction)
+        velocity = apply_drift_to(heading)
     if d < 0:
-        velocity = -new_heading * min(velocity.length(), max_speed_reverse)
-    rotation = new_heading.angle()
+        velocity = reverse_to(heading)
+    rotation = heading.angle()
     
-func apply_friction():
-    if velocity.length() < 5:
-        velocity = Vector2.ZERO
+
+func apply_drift_to(heading):
+    var traction = low_speed_traction if velocity.length() < slip_speed else high_speed_traction
+    var new_velocity = velocity.linear_interpolate(heading * velocity.length(), traction)
+    return new_velocity
+    
+func reverse_to(heading):
+    var new_velocity = -heading * min(velocity.length(), max_speed_reverse)
+    return new_velocity
+    
+func get_friction():
     var friction_force = velocity * friction
     var drag_force = velocity * velocity.length() * drag
-    acceleration += drag_force + friction_force
+    if velocity.length() < 100:
+        friction_force *= 3
+    return drag_force + friction_force
